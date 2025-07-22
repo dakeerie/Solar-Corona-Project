ced55aaf2b43258f47308840e9c414f863863b0b
Daniel Keerie <daniel@keerie.co.uk>
2025-07-22 13:22:57 +0100

    Results look good however need to work out velocity in km per second and radial distance in solar radii. Manages to solve around the critical point which is good

--- ParkerEq3-EVillegas.py
+++ ParkerEq3-EVillegas.py
@@ -18,10 +18,25 @@ rc = G*M_sun/(2*cs**2) #m
 
 #Dimensionless ODE function
 def Parker_ode(R, v):
-    numerator  = 2*(1/R - 1/(R**2))
-    denominator = (v - 1/v)
+    numerator  = 2*(1/R - 1/R**2)
+    denominator = (v-1/v)
     result = numerator/denominator
     return result
+
+def RK4(function, initial_conditions, x_final, n):
+    # RK4 solver
+    x = [initial_conditions[0]]
+    w = [initial_conditions[1]]
+    dx = (x_final - x[0]) / n # step
+    dx_2 = dx / 2 # half step
+    for i in range(n-1):
+        k1 = dx * function(x[i], w[i])
+        k2 = dx * function(x[i] + dx_2, w[i] + k1/2)
+        k3 = dx * function(x[i] + dx_2, w[i] + k2/2)
+        x.append(x[i] + dx) # x[i+1] = x[-1]
+        k4 = dx * function(x[-1], w[i] + k3)
+        w.append(w[i] + (k1 + 2*k2 + 2*k3 + k4)/6)
+    return x, w
 
 #Integration range
 r_min = R_sun #m
@@ -37,20 +52,18 @@ print(f'Sound speed = {cs/1e3} km/s')
 # print(f'v(r0) = {v0/1e3} km/s')
 
 #Solve
-sol_int = solve_ivp(Parker_ode, [R_min, 1], [1 - 1e-6], method = 'RK45')
-sol_ext = solve_ivp(Parker_ode, [1, R_max], [1 + 1e-6], method = 'RK45')
-#Extract solutions
-R_int = sol_int.t
-v_int = sol_int.y[0]
-R_ext = sol_ext.t
-v_ext = sol_ext.y[0]
-#Combine
-R_sol = np.concatenate((R_int, R_ext))
-v_sol = np.concatenate((v_int, v_ext))
+R_int, v_int = RK4(Parker_ode, [1, 1-1e-6], R_min, 300000)
+R_int.reverse()
+v_int.reverse()
+R_ext, v_ext = RK4(Parker_ode, [1, 1+1e-6], R_max, 600000)
+R_int.extend(R_ext)
+v_int.extend(v_ext)
+R_sol = R_int #dimensionless solutions
+v_sol = v_int #dimensionless solutions
 
 plt.plot(R_sol, v_sol)
 plt.xscale('log')
-plt.yscale('log')
+# plt.yscale('log')
 plt.show()
 
 
